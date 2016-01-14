8058b621f4b50afebd7f5f19517ca6194283f3f9
Akihiro Harai <jharai0815@gmail.com>
2016-01-14 18:05:25 +0900

    Make Stormpath work right

--- Gemfile
+++ Gemfile
@@ -3,7 +3,9 @@ ruby '2.2.3'
 
 gem 'rails', '4.2.3'
 gem 'rails_12factor', group: :production
-gem 'sdoc', '~> 0.4.0',          group: :doc
-gem 'spring',        group: :development
+gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'spring', group: :development
 gem 'puma'
 gem 'byebug', group: [:development, :test]
+gem 'stormpath-sdk', '>= 1.0.0.beta.9'
+gem 'dotenv-rails', groups: [:development, :test]
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,27 @@
 class WelcomeController < ApplicationController
   def hi
+    res = sp_client.accounts.get 'https://api.stormpath.com/v1/accounts/BR5Wfj3SzFKmwAQTS8SLK'
+    count = res.custom_data[:foobar]
+    res.custom_data[:foobar] += 1
+    res.custom_data.save
     respond_to do |format|
       format.json do
         render json: {
           status: "ok",
           message: "Success!",
+          count: count,
         }
       end
     end
   end
 
+  private
+
+  def sp_client
+    Stormpath::Client.new({
+       api_key: {
+         id: ENV['STORMPATH_ID'], secret: ENV['STORMPATH_SECRET'],
+       }
+    })
+  end
 end
