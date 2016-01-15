e92a993eb81d37ad0a9882dd74ec8b6e9f71917f
Akihiro Harai <jharai0815@gmail.com>
2016-01-15 14:56:49 +0900

    Change name to match this project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with(
-    name: ENV['ONEOP_BASIC_AUTH_NAME'],
-    password: ENV['ONEOP_BASIC_AUTH_PASSWORD'])
+    name: ENV['REGENT_BASIC_AUTH_NAME'],
+    password: ENV['REGENT_BASIC_AUTH_PASSWORD'])
 end
