84d80ef1cea58e6f65bbb5e11c055036561a333e
Akihiro Harai <jharai0815@gmail.com>
2016-01-15 13:53:53 +0900

    Reset process num to the default

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,11 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 1)
-threads_count = Integer(ENV['MAX_THREADS'] || 1)
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+port        ENV['PORT']     || 3001
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
